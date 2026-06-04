fa5db9703e3633358da590e75e1de31af14de8a6
abdelwahab-t <eng.abdelwahap.taha@gmail.com>
2026-06-04 04:14:30 +0300

    updating

--- src/Bootstrap/AppBootManager.php
+++ src/Bootstrap/AppBootManager.php
@@ -20,24 +20,34 @@ final readonly class AppBootManager
     public function boot(string $basePath, array $controllersDirs): void
     {
         foreach ($controllersDirs as $controllersDir) {
+            $path = $this->getFilePath($basePath, sprintf('/%s/*', $controllersDir));
+            $this->loadControllers($basePath, glob($path));
+        }
+    }
 
-            $path = $this->getFilePath(
-                $basePath,
-                sprintf('/%s/*.php', $controllersDir)
-            );
-
-            foreach (glob($path) as $file) {
+    /**
+     * @throws ReflectionException|ModuleClassNotFoundException
+     */
+    private function loadControllers(string $basePath, array $controllers): void
+    {
+        $separator = str_contains($basePath, '/') ? '/' : '\\';
+        foreach ($controllers as $file) {
+            if (is_dir($file)) {
+                $this->loadControllers($basePath, glob($file . $separator . '*'));
+                continue;
+            }
+            if (str_contains($file, '.php')) {
                 $this->registrar->register($file);
             }
-
         }
+
     }
 
     private function getFilePath(string $basePath, string $file): string
     {
         return $basePath . (str_contains($basePath, '/') ?
-            str_replace('\\', '/', $file) :
-            str_replace('/', '\\', $file));
+                str_replace('\\', '/', $file) :
+                str_replace('/', '\\', $file));
     }
 
 }
